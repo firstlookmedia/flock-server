d41ac8eaaa6c6658e1ab8d6ae74b26d2f56b9705
Micah Lee <micah@micahflee.com>
2019-04-03 16:50:45 -0700

    Support the gateway connecting to elasticsearch over both TLS and not TLS, so tests can run without generating certs

--- run_tests.sh
+++ run_tests.sh
@@ -9,8 +9,17 @@ echo
 
 printf "${BLUE}* Starting containers${NC}\n"
 docker network create testnet
-docker run -d --net testnet --name test-elasticsearch -e xpack.security.enabled=false -e transport.host=localhost elasticsearch:6.6.2@sha256:59dd45d05fe89cd713dfc20874c6298e1ec7eaf384e58410b677b9dead6986f1
-docker run -d --net testnet --name test-gateway -e ELASTICSEARCH_HOSTS=http://test-elasticsearch:9200 gateway
+docker run -d \
+  --net testnet \
+  --name test-elasticsearch \
+  -e xpack.security.enabled=false \
+  -e transport.host=localhost \
+  elasticsearch:6.6.2@sha256:59dd45d05fe89cd713dfc20874c6298e1ec7eaf384e58410b677b9dead6986f1
+docker run -d \
+  --net testnet \
+  --name test-gateway \
+  -e ELASTICSEARCH_HOSTS=http://test-elasticsearch:9200 \
+  gateway
 echo
 
 printf "${BLUE}* Waiting for elasticsearch${NC}\n"
