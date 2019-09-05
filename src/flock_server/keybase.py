428351aec46251e4b49fa3789f9c8f2f7c84ef11
Micah Lee <micah@micahflee.com>
2019-09-05 14:18:21 -0700

    Make the keybase bot connect and send a message to the channel

--- src/flock_server/keybase.py
+++ src/flock_server/keybase.py
@@ -12,18 +12,51 @@ class Handler:
         print(event)
 
 
-def start_keybase_bot():
+async def start(bot, channel):
+    await asyncio.gather(
+        bot.chat.send(channel, "My process just started :computer:"),
+        bot.start({
+            "local": False,
+            "wallet": False,
+            "dev": False,
+            "hide-exploding": False,
+            "filter_channel": channel
+        })
+    )
 
+
+def start_keybase_bot():
+    # Validation
+    validated = True
+    if not os.environ.get("KEYBASE_USERNAME"):
+        print("Error: KEYBASE_USERNAME must be set")
+        validated = False
+    if not os.environ.get("KEYBASE_PAPERKEY"):
+        print("Error: KEYBASE_PAPERKEY must be set")
+        validated = False
+    if not os.environ.get("KEYBASE_TEAM"):
+        print("Error: KEYBASE_TEAM must be set")
+        validated = False
+    if not os.environ.get("KEYBASE_CHANNEL"):
+        print("Error: KEYBASE_CHANNEL must be set")
+        validated = False
+    if not validated:
+        return
+
+    # Start the bot
     bot = Bot(
-        username=os.environ["KEYBASE_USERNAME"],
-        paperkey=os.environ["KEYBASE_PAPERKEY"],
+        username=os.environ.get("KEYBASE_USERNAME"),
+        paperkey=os.environ.get("KEYBASE_PAPERKEY"),
         handler=Handler()
     )
-    asyncio.run(bot.start({
-        "local": True,
-        "wallet": False,
-        "dev": True,
-        "hide-exploding": False,
-        "filter_channel": None,
-        "filter_channels": None,
-    }))
+    channel = {
+        "name": os.environ.get("KEYBASE_TEAM"),
+        "topic_name": os.environ.get("KEYBASE_CHANNEL"),
+        "members_type": "team"
+    }
+
+    asyncio.run(start(bot, channel))
+
+
+if __name__ == '__main__':
+    start_keybase_bot()
